f930c6896a771c60c92ffb404a825f87a23c511e
CaoKao <bksaechao@gmail.com>
2019-10-11 15:41:17 -0700

    created a random number generator for each crystal

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,9 @@
 //Starting variables
-let ranNum = 0;
 let win = 0;
 let loss = 0;
 let totScore = 0;
 
 //Grabbing elements
-$("#rNumber").html(ranNum);
 $("#w").html(win);
 $("#l").html(loss);
 $("#totalScore").html(totScore);
@@ -20,9 +18,24 @@ $("#totalScore").css("color", "red");
 $("#totalScore").css("background-color", "#ffffff");
 $("#totalScore").css("font-size", "30px");
 
-//Random number is generated when the document is loaded
+//Random numbers are generated when the webpage is loaded
 $(document).ready(function () {
+    //Computer random number
     var numGen = Math.floor(Math.random() * 71) + 30;
     $("#rNumber").html(numGen);
     console.log(numGen);
+
+    //Random number for each crystal
+    var crys1 = Math.floor(Math.random() * 10) + 1
+    $("#blueCrystal").html(crys1);
+    console.log(crys1);
+    var crys2 = Math.floor(Math.random() * 10) + 1
+    $("#redCrystal").html(crys2);
+    console.log(crys2);
+    var crys3 = Math.floor(Math.random() * 10) + 1
+    $("#greenCrystal").html(crys3);
+    console.log(crys3);
+    var crys4 = Math.floor(Math.random() * 10) + 1
+    $("#purpleCrystal").html(crys4);
+    console.log(crys4);
 });
