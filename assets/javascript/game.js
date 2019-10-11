332d763979a0bbcbd9562dff27a65ba9133ac9a2
CaoKao <bksaechao@gmail.com>
2019-10-11 16:17:35 -0700

    changed the range of numbers for each crystal number generator to prevent duplicate numbers

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,21 +21,32 @@ $("#totalScore").css("font-size", "30px");
 //Random numbers are generated when the webpage is loaded
 $(document).ready(function () {
     //Computer random number
-    var numGen = Math.floor(Math.random() * 71) + 30;
+    var numGen = Math.floor(Math.random() * 121) + 30;
     $("#rNumber").html(numGen);
     console.log(numGen);
 
     //Random number for each crystal
-    var crys1 = Math.floor(Math.random() * 10) + 1
+    var crys1 = Math.floor(Math.random() * 6) + 35
     $("#blueCrystal").html(crys1);
     console.log(crys1);
-    var crys2 = Math.floor(Math.random() * 10) + 1
+    var crys2 = Math.floor(Math.random() * 11) + 12
     $("#redCrystal").html(crys2);
     console.log(crys2);
     var crys3 = Math.floor(Math.random() * 10) + 1
     $("#greenCrystal").html(crys3);
     console.log(crys3);
-    var crys4 = Math.floor(Math.random() * 10) + 1
+    var crys4 = Math.floor(Math.random() * 9) + 24
     $("#purpleCrystal").html(crys4);
     console.log(crys4);
+
+    //if statement to prevent duplicate numbers(doesn't seem to work lol)
+    // if (crys1 === crys2 || crys1 === crys3 || crys1 === crys4) {
+    //     crys1;
+    // } else if (crys2 === crys1 || crys2 === crys3 || crys2 === crys4) {
+    //     crys2;
+    // } else if (crys3 === crys1 || crys3 === crys2 || crys3 === crys4) {
+    //     crys3;
+    // } else if (crys4 === crys1 || crys4 === crys2 || crys4 === crys3) {
+    //     crys4;
+    // }
 });
