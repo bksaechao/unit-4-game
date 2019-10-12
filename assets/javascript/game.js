971652f9c4f67ad2b40109b98eabbcca8ee2932e
CaoKao <bksaechao@gmail.com>
2019-10-12 02:59:45 -0700

    changed the computer number to pick between 19 and 120 and cyrstal numbers to pick between 1-12

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,21 +21,21 @@ $("#totalScore").css("font-size", "30px");
 //Random numbers are generated when the webpage is loaded
 $(document).ready(function () {
     //Computer random number
-    var numGen = Math.floor(Math.random() * 121) + 30;
+    var numGen = Math.floor(Math.random() * 101) + 19;
     $("#rNumber").html(numGen);
     console.log(numGen);
 
     //Random number for each crystal
-    var crys1 = Math.floor(Math.random() * 6) + 35
+    var crys1 = Math.floor(Math.random() * 12) + 1
     $("#blueCrystal").html(crys1);
     console.log(crys1);
-    var crys2 = Math.floor(Math.random() * 11) + 12
+    var crys2 = Math.floor(Math.random() * 12) + 1
     $("#redCrystal").html(crys2);
     console.log(crys2);
-    var crys3 = Math.floor(Math.random() * 10) + 1
+    var crys3 = Math.floor(Math.random() * 12) + 1
     $("#greenCrystal").html(crys3);
     console.log(crys3);
-    var crys4 = Math.floor(Math.random() * 9) + 24
+    var crys4 = Math.floor(Math.random() * 12) + 1
     $("#purpleCrystal").html(crys4);
     console.log(crys4);
 
