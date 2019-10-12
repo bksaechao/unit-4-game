afc14c5506d8159a9000978f480e084d31abd2ff
CaoKao <bksaechao@gmail.com>
2019-10-12 04:36:02 -0700

    created the win conditions using an if statement

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -26,23 +26,80 @@ $(document).ready(function () {
     console.log(numGen);
 
     //Random number for each crystal
-    var crys1 = Math.floor(Math.random() * 11) + 1
+    var crys1 = Math.floor(Math.random() * 12) + 1
     $("#blueCrystal").html(crys1);
     console.log(crys1);
-    var crys2 = Math.floor(Math.random() * 11) + 1
+    var crys2 = Math.floor(Math.random() * 12) + 1
     $("#redCrystal").html(crys2);
     console.log(crys2);
-    var crys3 = Math.floor(Math.random() * 11) + 1
+    var crys3 = Math.floor(Math.random() * 12) + 1
     $("#greenCrystal").html(crys3);
     console.log(crys3);
-    var crys4 = Math.floor(Math.random() * 11) + 1
+    var crys4 = Math.floor(Math.random() * 12) + 1
     $("#purpleCrystal").html(crys4);
     console.log(crys4);
 
+    //Adds value to the total score
     $("#blueCrystal").on("click", function () {
-        currentScore = totScore + crys1;
-        console.log(currentScore);
-        $("#totalScore").html(currentScore);
+        totScore += crys1;
+        console.log(totScore);
+        $("#totalScore").html(totScore);
 
+        //win/loss conditions
+        if (totScore > numGen) {
+            alert("You Lose!")
+            loss++;
+            $("#l").html(loss);
+        } else if (totScore === numGen) {
+            alert("You Win!")
+            win++;
+            $("#w").html(win);
+        }
+    })
+
+    $("#redCrystal").on("click", function () {
+        totScore += crys2;
+        console.log(totScore);
+        $("#totalScore").html(totScore);
+        if (totScore > numGen) {
+            alert("You Lose!")
+            loss++;
+            $("#l").html(loss);
+        } else if (totScore === numGen) {
+            alert("You Win!")
+            win++;
+            $("#w").html(win);
+        }
+    })
+
+    $("#greenCrystal").on("click", function () {
+        totScore += crys3;
+        console.log(totScore);
+        $("#totalScore").html(totScore);
+
+        if (totScore > numGen) {
+            alert("You Lose!")
+            loss++;
+            $("#l").html(loss);
+        } else if (totScore === numGen) {
+            alert("You Win!")
+            win++;
+            $("#w").html(win);
+        }
+    })
+    $("#purpleCrystal").on("click", function () {
+        totScore += crys4;
+        console.log(totScore);
+        $("#totalScore").html(totScore);
+
+        if (totScore > numGen) {
+            alert("You Lose!")
+            loss++;
+            $("#l").html(loss);
+        } else if (totScore === numGen) {
+            alert("You Win!")
+            win++;
+            $("#w").html(win);
+        }
     })
 });
